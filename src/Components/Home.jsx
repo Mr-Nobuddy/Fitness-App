e6d04a8f34a0f030302d0f5a0e168ab681cfb15f
divyansh1733 <divyanshk072@gmail.com>
2024-04-07 12:22:46 +0530

    initial commit

---
+++ src/Components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from './NavBar'
+import Box from "@mui/material/Box";
+
+const Home = () => {
+  return (
+    <Box sx={{margin:0,backgroundColor:"yellow",padding:"20px",paddingTop:"10px"}}>
+      <NavBar/>
+      
+    </Box>
+  )
+}
+
+export default Home
