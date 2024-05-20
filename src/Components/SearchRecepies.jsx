b12d53dff9819aa454791404ad3f69dd5a801767
divyansh1733 <divyanshk072@gmail.com>
2024-05-21 00:55:37 +0530

    added recepies page and recepies api

--- src/Components/SearchRecepies.jsx
+++ src/Components/SearchRecepies.jsx
@@ -1,18 +1,59 @@
-import { Box, Button, TextField } from '@mui/material'
-import React from 'react'
-import NavBar from './NavBar'
+import {
+  Box,
+  Button,
+  CircularProgress,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
+import NavBar from "./NavBar";
+import axios from "axios";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 
 const SearchRecepies = () => {
+  const [recepies, setRecepies] = useState([]);
+  const [searchRec, setSearchRec] = useState("");
+  const [showLoader, setShowLoader] = useState(false);
+  const [show, setShow] = useState(false);
+
+  const options = {
+    method: "GET",
+    url: "https://recipe-by-api-ninjas.p.rapidapi.com/v1/recipe",
+    params: {
+      query: searchRec,
+    },
+    headers: {
+      "X-RapidAPI-Key": "f775f70b37msh97fa7ed0c6cd78cp17d0a4jsn0725e9aaad31",
+      "X-RapidAPI-Host": "recipe-by-api-ninjas.p.rapidapi.com",
+    },
+  };
+  const handleSearch = async () => {
+    try {
+      setShowLoader(true);
+      setShow(false);
+      const response = await axios.request(options);
+      // console.log(response.data);
+      setShowLoader(false);
+      setRecepies(response.data);
+      setShow(true);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <Box sx={{ backgroundColor: "yellow", padding: "10px", height: "200vh" }}>
-      <NavBar/>
+      <NavBar />
       <Box
         sx={{
           display: "flex",
           alignItems: "center",
           gap: 2,
           justifyContent: "center",
-          marginBottom: "10px",
+          marginBottom: "20px",
           marginTop: "20px",
         }}
       >
@@ -26,6 +67,7 @@ const SearchRecepies = () => {
             "& input": { color: "black" },
             "&:hover": { borderColor: "black" },
           }}
+          onChange={(e) => setSearchRec(e.target.value)}
         />
         <Button
           variant="contained"
@@ -34,12 +76,50 @@ const SearchRecepies = () => {
             "&:hover": { backgroundColor: "black", color: "#fff" },
             transition: "0.2s ease-in-out",
           }}
+          onClick={handleSearch}
         >
           Search
         </Button>
       </Box>
+      <Box
+        sx={{
+          display: showLoader ? "flex" : "none",
+          width: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <CircularProgress />
+      </Box>
+      
+        {show ? (
+          recepies.map((item) => (
+            <Accordion sx={{marginBottom:"20px"}}>
+              <AccordionSummary
+                expandIcon={<ArrowDownwardIcon sx={{color:"white"}}/>}
+                aria-controls="panel1-content"
+                id="panel1-header"
+                sx={{backgroundColor:"#000",color:"#fff"}}
+              >
+                <Typography sx={{fontSize:"2em"}}>{item.title}</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Box>
+                  <Typography sx={{fontSize:"1.3em",fontWeight:800,marginBottom:"10px"}}>Ingredients</Typography>
+                  <Typography sx={{marginBottom:"20px"}}>{item.ingredients}</Typography>
+                </Box>
+                <Box>
+                  <Typography sx={{fontSize:"1.3em",fontWeight:800,marginBottom:"10px"}}>Instructions</Typography>
+                  <Typography sx={{marginBottom:"20px"}}>{item.instructions}</Typography>
+                </Box>
+              </AccordionDetails>
+            </Accordion>
+          ))
+        ) : (
+          <></>
+        )}
     </Box>
-  )
-}
+  );
+};
 
-export default SearchRecepies
+export default SearchRecepies;
